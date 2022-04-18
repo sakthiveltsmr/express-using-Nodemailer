0d64484ae0f7c7aac60b405e0d264688a4cc61b3
sakthiveltsmr <sakthiveltsmr@gmail.com>
2022-04-18 22:17:51 +0530

    app

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 const mailer = require("nodemailer");
 
-const MailOptions = mailer.createTransport({});
+const MailOptions = mailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "sakthiveltest1234@gmail.com",
+    pwd: "Sakthi1234#",
+  },
+});
+var mailoptions = {
+  from: "sakthiveltest1234@gmail.com",
+  to: "sakthiveltsmr@gmail.com",
+  subject: "learning in nodejs",
+  text: "email send successfully",
+};
+
+console.log("hello");
